8b8fcdcc88c0e22d55a8eff31867d87c12ce4cb9
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-09-26 07:38:49 -0300

    Update salasDeAulaModel.js

--- models/salasDeAulaModel.js
+++ models/salasDeAulaModel.js
@@ -2,7 +2,7 @@ const { salasDeAula, getId } = require('../db/database');
 
 class SalaDeAula {
   constructor(descricao, localizacao, capacidade) {
-    this.salasdeaulaid = getId(); // Chama a função para obter um novo ID
+    this.salasdeaulaid = getId(); 
     this.descricao = descricao;
     this.localizacao = localizacao;
     this.capacidade = capacidade;
